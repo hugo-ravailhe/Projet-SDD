31b0c52a49ff574788f5817abdba13426bd9a765
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-11-28 19:38:54 +0100

    Prototype Suppression
    Prototype de Fonction de Suppression

---
+++ Fonctions/suppression.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+#include "suppression.h"
+
+void SuppressionCode(Code **C){
+
+}
+
+void SuppressionListCode(ListCode **L){
+
+}
+
+void SuppressionList(List **L){
+
+}
+
+void SuppressionNode(Node **B){
+
+}
+
+void SuppressionNodeCode(NodeCode **B){
+
+}
+
+void SuppressionListNode(ListNode **L){
+
+}
---
+++ Fonctions/suppression.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_C_SUPPRESSION_H
+#define MAIN_C_SUPPRESSION_H
+
+void SuppressionCode(Code **C);
+
+void SuppressionListCode(ListCode **L);
+
+void SuppressionList(List **L);
+
+void SuppressionNode(Node **B);
+
+void SuppressionNodeCode(NodeCode **B);
+
+void SuppressionListNode(ListNode **L);
+
+#endif //MAIN_C_SUPPRESSION_H
