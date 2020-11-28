06da484fc0cc893c053ad0a5b7ea9c67e37e240b
ArthurLoot <72505016+ArthurLoot@users.noreply.github.com>
2020-11-29 00:49:38 +0100

    remplissage fct

--- Fonctions/print_structures.c
+++ Fonctions/print_structures.c
@@ -4,29 +4,93 @@
 #include "print_structures.h"
 
 void PrintListCode(ListCode *L){
-
+    while(L != NULL){
+        printf("%c ", L->data);
+        L = L->next;
+    }
+    printf("NULL\n");
 }
 
 void PrintTabChar(char* tab){
-
+    int i = 0;
+    while (tab[i] != '\0'){
+        printf("%c", tab[i]);
+        i++;
+    }
 }
 
 void PrintCode(Code* C){
-
+    while(C != NULL){
+        printf("%c ", C->data);
+        C = C->next;
+    }
+    printf("NULL\n");
 }
 
 void PrintList(List* L){
-
+    while(L != NULL){
+        printf("( %c, %d ) -> ", L->data, L->occu);
+        L = L->next;
+    }
+    printf("NULL\n");
 }
 
 void PrintListNode(ListNode* L){
-
+    while(L != NULL){
+        printf("( %c, %d ) -> ", L->data->data, L->data->occu);
+        L = L->next;
+    }
+    printf("NULL\n");
 }
 
-void PrintABR(Node* B){
+void printABR(Node *B, int x){
+    int i;
+    for(i = 0; i < x; i++){
+        printf("\t");
+    }
+    if(x != 0)
+        printf("|---");
+    if(B == NULL){
+        printf(" NULL \n");
+    }else if(B->data != '\0'){
+        printf(" %c, %d \n", B->data, B->occu);
+        printABR(B->left, x + 1);
+        printABR(B->right, x + 1);
+    } else{
+        printf(" NULL, %d \n", B->occu);
+        printABR(B->left, x + 1);
+        printABR(B->right, x + 1);
+    }
+}
 
+void PrintTree(Node* B){
+    if (B != NULL)
+        printABR(B, 0);
+    else
+        printf("Arbre vide");
+}
+
+void printABRCode(NodeCode *B, int x){
+    int i;
+    for(i = 0; i < x; i++){
+        printf("\t");
+    }
+    if(x != 0)
+        printf("|---");
+    if(B == NULL){
+        printf(" NULL \n");
+    }else if(B->data != '\0'){
+        printf(" %c, ", B->data);
+        PrintCode(B->code);
+        printf("\n");
+        printABRCode(B->left, x + 1);
+        printABRCode(B->right, x + 1);
+    }
 }
 
 void PrintABRCode(NodeCode *B){
-
+    if (B != NULL)
+        printABRCode(B, 0);
+    else
+        printf("Arbre vide");
 }
--- Fonctions/suppression.c
+++ Fonctions/suppression.c
@@ -4,25 +4,64 @@
 #include "suppression.h"
 
 void SuppressionCode(Code **C){
-
+    Code *D, *E;
+    D = *C;
+    while (D != NULL){
+        E = D;
+        D = D->next;
+        free(E);
+        E = NULL;
+    }
 }
 
 void SuppressionListCode(ListCode **L){
-
+    ListCode *J, *K;
+    J = *L;
+    while (J != NULL){
+        K = J;
+        J = J->next;
+        SuppressionCode(&K->code);
+        free(K);
+        K = NULL;
+    }
 }
 
 void SuppressionList(List **L){
-
+    List *J, *K;
+    J = *L;
+    while (J != NULL){
+        K = J;
+        J = J->next;
+        free(K);
+        K = NULL;
+    }
 }
 
 void SuppressionNode(Node **B){
-
+    if (*B != NULL){
+        SuppressionNode(&(*B)->left);
+        SuppressionNode(&(*B)->left);
+        free(B);
+    }
 }
 
 void SuppressionNodeCode(NodeCode **B){
-
+    if (*B != NULL){
+        SuppressionNodeCode(&(*B)->left);
+        SuppressionNodeCode(&(*B)->left);
+        SuppressionCode(&(*B)->code);
+        free(B);
+    }
 }
 
 void SuppressionListNode(ListNode **L){
-
+    ListNode *J, *K;
+    J = *L;
+    while (J != NULL){
+        K = J;
+        J = J->next;
+        SuppressionNode(&K->data);
+        free(K);
+        K = NULL;
+    }
 }
