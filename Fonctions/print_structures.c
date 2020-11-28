50c907ac3b8d368380ea6061d0683de541ca25af
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-11-28 19:38:35 +0100

    Prototype Print
    Prototype de Fonctions de Print

---
+++ Fonctions/print_structures.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+#include "print_structures.h"
+
+void PrintListCode(ListCode *L){
+
+}
+
+void PrintTabChar(char* tab){
+
+}
+
+void PrintCode(Code* C){
+
+}
+
+void PrintList(List* L){
+
+}
+
+void PrintListNode(ListNode* L){
+
+}
+
+void PrintABR(Node* B){
+
+}
+
+void PrintABRCode(NodeCode *B){
+
+}
---
+++ Fonctions/print_structures.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_C_PRINT_STRUCTURES_H
+#define MAIN_C_PRINT_STRUCTURES_H
+
+void PrintListCode(ListCode *L);
+
+void PrintTabChar(char* tab);
+
+void PrintCode(Code* C);
+
+void PrintList(List* L);
+
+void PrintListNode(ListNode* L);
+
+void PrintABR(Node* N);
+
+void PrintABRCode(NodeCode *B);
+
+#endif //MAIN_C_PRINT_STRUCTURES_H
