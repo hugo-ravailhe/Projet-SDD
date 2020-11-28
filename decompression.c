29095caaf291f940eb0b7898ee7d594aa7386e6b
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-11-28 19:36:27 +0100

    Prototype Decompression
    Prototype de Fonction de Decompression

---
+++ decompression.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+#include "decompression.h"
+#include "Fonctions/creation.h"
+#include "Fonctions/AVL.h"
+#include "Fonctions/fonctions.h"
+#include "Fonctions/print_structures.h"
+
+///Decodage du dico
+Node* CreationHuffmanTreeByDico(const char* dico, int *i, char c, Node *B){
+
+}
+
+Node* DecodeDico(const char* dico){
+
+}
+
+char HuffmanChar(Node *B, char* binary, int *i){
+
+}
+
+char* DecodeBinary(Node *B, char* binary){
+
+}
+
+void Decompression(char* bin, char* dico){
+
+}
---
+++ decompression.h
@@ -0,0 +1,14 @@
+#ifndef TEST_DECOMPRESSION_H
+#define TEST_DECOMPRESSION_H
+
+Node* CreationHuffmanTreeByDico(const char* dico, int *i, char c, Node *B);
+
+Node* DecodeDico(const char* dico);
+
+char HuffmanChar(Node *B, char* binary, int *i);
+
+char* DecodeBinary(Node *B, char* binary);
+
+void Decompression(char* bin, char* dico);
+
+#endif //TEST_DECOMPRESSION_H
