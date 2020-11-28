c90e27a0c161acc97a68398b5f6693f2d53aa81e
mrkeinzi <59828978+mrkeinzi@users.noreply.github.com>
2020-11-28 19:37:21 +0100

    Prototype Creation
    Prototype de Fonction de Creation

---
+++ Fonctions/creation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+#include "creation.h"
+
+List* CreationList(char c){
+
+}
+
+Node* CreationNode(char c, int occu){
+
+}
+
+ListNode* CreationListNode(char c, int occu){
+
+}
+
+ListCode* CreationListCode(char c, Code* code){
+
+}
+
+NodeCode* CreationNodeCode(char c, Code* code){
+
+}
+
+Code* CreationCode(char c){
+
+}
---
+++ Fonctions/creation.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_C_CREATION_H
+#define MAIN_C_CREATION_H
+
+List* CreationList(char c);
+
+Node* CreationNode(char c, int occu);
+
+ListNode* CreationListNode(char c, int occu);
+
+ListCode* CreationListCode(char c, Code* code);
+
+NodeCode* CreationNodeCode(char c, Code* code);
+
+Code* CreationCode(char c);
+
+#endif //MAIN_C_CREATION_H
